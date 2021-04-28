3307c55b05b527ed91c1bbfe3f5670d4763b3cea
Ramiro <Ramiroadal626@gmail.com>
2021-04-28 00:43:15 -0300

    funciones

--- app.js
+++ app.js
@@ -1,1 +1,18 @@
 alert("hola")
+
+function sumar(a,b){
+	return a + b;
+}
+function restar (a,b){
+	return a - b;
+}
+function multiplicar (a,b){
+	return a * b;
+}
+function division(a,b){
+	return a / b;
+}
+console.log(sumar(10,10))
+console.log(restar(20,15))
+console.log(multiplicar(4,6))
+console.log (division(20,5))
