5306beb8ddc99faba8cb701b00cc46b24499e665
Rodrigo Ramirez <rramirez.dev@gmail.com>
2021-05-04 00:49:27 -0300

    agrego js

--- app.js
+++ app.js
@@ -16,3 +16,18 @@ function dividir(a,b){
 function logear(a){
 	console.log(a);
 }
+
+//comentarios:
+//che hago este codigo rancio asi, porque x persona me dijo
+//camelcase: Javascript
+//var botonUno = 
+
+//pascalCase: C#
+//var BotonUno = 
+
+//snake_case: Python
+//var boton_uno
+
+var botonUno = document.getElementById('boton-uno');
+console.log(botonUno);
+
