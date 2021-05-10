80a7d90df05c74703066311e46b4f4f56a321154
Julián <Alvarezjulian.jaa1@gmail.com>
2021-05-10 12:21:40 -0300

    Cambios numerazos

--- calculadora.js
+++ calculadora.js
@@ -2,6 +2,6 @@ alert("chau")
 
 function escribirNumerazos(numerazo){
 	var pantalla = document.getElementById('pantalla');
-	pantalla.innerText = pantalla.innertext + numerazo;
+	pantalla.innerText = pantalla.innerText += numerazo;
 	console.log(pantalla.innerText);
 }
